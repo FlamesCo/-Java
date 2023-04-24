31b7bcdcb59b62a560bb32d640d675010c61cf17
Flames Elite <rinagunnerx@gmail.com>
2023-04-24 03:19:01 -0700

    Create CalcCLI-X.X.X.java

---
+++ CalcCLI-X.X.X.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class CalcCLI-X.X.X {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What would you like to do?");
+        System.out.println("1. Add");
+        System.out.println("2. Subtract");
+        System.out.println("3. Multiply");
+        System.out.println("4. Divide");
+
+        int choice = scanner.nextInt();
+
+        System.out.println("Enter the first number:");
+        double num1 = scanner.nextDouble();
+
+        System.out.println("Enter the second number:");
+        double num2 = scanner.nextDouble();
+
+        double result = 0;
+
+        switch (choice) {
+            case 1:
+                result = num1 + num2;
+                break;
+            case 2:
+                result = num1 - num2;
+                break;
+            case 3:
+                result = num1 * num2;
+                break;
+            case 4:
+                result = num1 / num2;
+                break;
+            default:
+                System.out.println("Invalid choice!");
+                return;
+        }
+
+        System.out.println("The result is: " + result);
+    }
+}
